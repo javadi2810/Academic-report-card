64ac1efd387a1dfcc5fc6af6ec4ef6d4001a6966
mohammadkhaliljavdi <mohammadkhaliljavadi@gmail.com>
2026-01-03 12:08:36 +0330

    I edited the JS code from the basic method to my final method to make it better and more upgradable.

--- script/script.js
+++ script/script.js
@@ -29,49 +29,48 @@ let formModal
 
 
 
-
-let InformationButton = document.querySelectorAll("#Information-form button");
-InformationButton.forEach(btn => {
-    btn.addEventListener("click", function (e) {
-        e.preventDefault();
-        if (this.classList.contains("login-student-submit")) {
-            formRotate.classList.add("rotat")
-            formContainer.classList.add("rotat")
-
-
-            modalRotation.style.opacity = "0";
-            setTimeout(() => {
-                modalRotation.style.opacity = "1";
-            }, 1000)
+const actions = {
+    "login-student-submit": () => {
+        rotateForm(true);
+        formToggle(formStudent, formSchool)
+    },
+    "login-school-close": () => {
+        rotateForm(false);
+        formToggle(formSchool, formStudent)
+    },
+    "login-school-submit":()=>{
+        Converting()
+    }
 
 
-            setTimeout(() => {
-                formStudent.classList.remove("active");
-                formSchool.classList.add("active")
-            }, 300)
-
-        }
+}
 
 
-        if (this.classList.contains("login-school-close")) {
-            formRotate.classList.remove("rotat")
-            formContainer.classList.remove("rotat")
+
+function rotateForm(add) {
+    formRotate.classList[add ? "add" : "remove"]("rotat");
+    formContainer.classList[add ? "add" : "remove"]("rotat");
+
+    modalRotation.style.opacity = "0";
+    setTimeout(() => {
+        modalRotation.style.opacity = "1";
+    }, 1000)
+}
 
 
-            modalRotation.style.opacity = "0";
-            setTimeout(() => {
-                modalRotation.style.opacity = "1";
-            }, 1000)
+
+function formToggle(hidenForm,showForm) {
+    setTimeout(() => {
+        hidenForm.classList.remove("active")
+        showForm.classList.add("active");
+    }, 300)
+}
 
 
-            setTimeout(() => {
-                formStudent.classList.add("active");
-                formSchool.classList.remove("active")
-            }, 300)
-        }
 
-        if (this.classList.contains("login-school-submit")) {
-            // Converting a login form to a medal form ------------------------------
+
+function Converting(){
+    //   Converting a login form to a medal form ------------------------------
             formLogin.classList.add("form-modal")
             formLogin.classList.remove("form-login")
             formModal = document.querySelector('.form-modal')
@@ -82,26 +81,90 @@ InformationButton.forEach(btn => {
             formContainer.classList.remove("login-container")
 
 
-
-
             // buttons-----------------------------------------------
-            login_student_submit.classList.add("modal-student-submit")
-            login_student_submit.classList.remove("login-student-submit")
+            login_student_submit.classList.replace("login-student-submit", "modal-student-submit");
 
+            login_student_submit.classList.replace("login-student-close", "modal-student-close");
 
-            login_student_close.classList.add("modal-student-close")
-            login_student_close.classList.remove("login-student-close")
+            login_school_submit.classList.replace("login-school-submit","modal-school-submit")
 
-            login_school_submit.classList.add("modal-school-submit")
-            login_school_submit.classList.remove("login-school-submit")
-
-            login_school_close.classList.add("modal-school-close")
-            login_school_close.classList.remove("login-school-close")
-
-
-
+            login_school_close.classList.replace("login-school-close","modal-school-close")
+            
+            
             modal.classList.remove("active")
-        }
+}
+
+
+let InformationButton = document.querySelectorAll("#Information-form button");
+InformationButton.forEach(btn => {
+    // The beginner method I used at first-----------------------------------------------------
+    // btn.addEventListener("click", function (e) {
+    //     e.preventDefault();
+    //     if (this.classList.contains("login-student-submit")) {
+    //         formRotate.classList.add("rotat")
+    //         formContainer.classList.add("rotat")
+
+
+    //         modalRotation.style.opacity = "0";
+    //         setTimeout(() => {
+    //             modalRotation.style.opacity = "1";
+    //         }, 1000)
+
+
+    //         setTimeout(() => {
+    //             formStudent.classList.remove("active");
+    //             formSchool.classList.add("active")
+    //         }, 300)
+
+    //     }
+
+
+    //     if (this.classList.contains("login-school-close")) {
+    //         formRotate.classList.remove("rotat")
+    //         formContainer.classList.remove("rotat")
+
+
+    //         modalRotation.style.opacity = "0";
+    //         setTimeout(() => {
+    //             modalRotation.style.opacity = "1";
+    //         }, 1000)
+
+
+    //         setTimeout(() => {
+    //             formStudent.classList.add("active");
+    //             formSchool.classList.remove("active")
+    //         }, 300)
+    //     }
+
+    //     if (this.classList.contains("login-school-submit")) {
+    //         Converting()
+
+    //         modal.classList.remove("active")
+    //     }
+
+    // })
+
+
+
+
+
+
+
+    // The final method ---------------------------------------------------------------------------------------
+    btn.addEventListener("click",(e)=> {
+        e.preventDefault()
+        Object.keys(actions).forEach((ke)=>{
+           if(btn.classList.contains(ke)){
+            console.log("سلام");
+            actions[ke]()
+           }
+
+        
+       })
 
     })
 })
+
+
+
+
